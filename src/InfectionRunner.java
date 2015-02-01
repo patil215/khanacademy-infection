f73e5c00df50c696c3715d4db89d1e79e48cf130
patil215 <neilpatil215@gmail.com>
2015-02-01 13:19:21 -0600

    Basic panel for visualization

--- src/InfectionRunner.java
+++ src/InfectionRunner.java
@@ -1,8 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Random;
 
 public class InfectionRunner {
 
+    private static JFrame frame;
+    private static JPanel controlsPanel;
+
+    private static JLabel numberUsersLabel;
+    private static JLabel numberRelationshipsLabel;
+    private static JLabel limitedInfectionTargetLabel;
+
+    private static JTextField numberUsersText;
+    private static JTextField numberRelationshipsText;
+    private static JTextField limitedInfectionTargetText;
+
+    private static JButton generateButton;
+    private static JButton infectButton;
+    private static JButton resetButton;
+
     public static void main(String args[]) {
+        runSimulation();
+
+        generateFrame();
+    }
+
+    private static void generateFrame() {
+        frame = new JFrame("Khan Academy Infections - Neil Patil");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700, 700);
+        frame.setLocationRelativeTo(null);
+
+        Container contents = frame.getContentPane();
+        contents.setLayout(new BoxLayout(contents, BoxLayout.PAGE_AXIS));
+
+        controlsPanel = new JPanel(new FlowLayout());
+
+        numberUsersLabel = new JLabel("Number of users:");
+        numberRelationshipsLabel = new JLabel("Number of student/coach relationships:");
+        limitedInfectionTargetLabel = new JLabel("Limited infection target number:");
+
+        numberUsersText = new JTextField(4);
+        numberRelationshipsText = new JTextField(4);
+        limitedInfectionTargetText = new JTextField(4);
+
+        generateButton = new JButton("Generate");
+        infectButton = new JButton("Infect");
+        resetButton = new JButton("Reset");
+
+        controlsPanel.add(numberUsersLabel);
+        controlsPanel.add(numberUsersText);
+        controlsPanel.add(numberRelationshipsLabel);
+        controlsPanel.add(numberRelationshipsText);
+        controlsPanel.add(limitedInfectionTargetLabel);
+        controlsPanel.add(limitedInfectionTargetText);
+        controlsPanel.add(generateButton);
+        controlsPanel.add(infectButton);
+        controlsPanel.add(resetButton);
+
+        frame.add(controlsPanel);
+        frame.setVisible(true);
+    }
+
+    private static void runSimulation() {
         int numUsers = 10;
         int numRelationships = 3;
         int versionToInfect = 2;
